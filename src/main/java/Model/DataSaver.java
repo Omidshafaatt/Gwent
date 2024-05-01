ce60011a5fdbaadb858c33cf21e6130e8badec74
JavadiSM <1remehr@gmail.com>
2024-05-01 13:24:40 +0330

    data saver created

---
+++ src/main/java/Model/DataSaver.java
@@ -0,0 +1,43 @@
+package Model;
+
+import Model.Card.Card;
+import Model.Game.GameObject;
+import Model.User.User;
+
+import java.util.ArrayList;
+
+public class DataSaver {
+    public static void gameSaver(GameObject gameObject) {
+
+    }
+
+    public static void userSaver(User user) {
+
+    }
+
+    public static void updateUserName(String oldUsername, String newUsername, User user) {
+
+    }
+
+    public static void updateDeck(ArrayList<Card> deck, String faction, String address, User user) {
+        
+    }
+
+    public static void userDeckSaver(ArrayList<Card> deck, String faction, String address, User user) {
+
+    }
+
+    public static void userDeckSaver(ArrayList<Card> deck, String faction, String address, String username) {
+
+    }
+
+    public static void securityAnswersSaver(String answer, User user) {
+
+    }
+
+    public static void cardSaver(Card card) {
+
+    }
+
+
+}
