5f009b93d8581b35ecb728ada9c4e32e1104e213
Saleh-kgh <tavilisaleh@gmail.com>
2024-05-01 11:45:34 +0330

    added preGameController and additional fields and methods to user and userInventory models

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,4 +1,12 @@
 package controller;
 
+import Model.Result;
+import Model.User.User;
+
 public class MainController {
+
+    public Result createGameWithPlayer(User user) {
+        Result result = new Result ("no", user.isPlayingGame());
+        return null;
+    }
 }
