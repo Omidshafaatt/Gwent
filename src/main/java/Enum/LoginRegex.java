90a93aed2301182660b7bc38824387154ba80ec6
JavadiSM <1remehr@gmail.com>
2024-05-25 15:32:50 +0330

    Game history,LoginController,LoginRegex,Player,RegisterController and userfaction updated

--- src/main/java/Enum/LoginRegex.java
+++ src/main/java/Enum/LoginRegex.java
@@ -1,5 +1,20 @@
 package Enum;
 
-public enum LoginRegex {
+import java.util.regex.Pattern;
 
+public enum LoginRegex {
+    VALID_USERNAME("[a-zA-Z0-9-]+"),
+    VALID_PASSWORD("[!@#$%^&*()-_=+0-9a-zA-Z]+"),
+    STRONG_PASSWORD("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_=+]).{8,}$"),
+    VALID_EMAIL(".*@*\\.com");
+
+    private final Pattern pattern;
+
+    LoginRegex(String regex) {
+        this.pattern = Pattern.compile(regex);
+    }
+
+    public boolean matches(String input) {
+        return pattern.matcher(input).matches();
+    }
 }
